88fdf687f1c82cefceee65dfe87dccb48900e207
HitriyEvgenii <moskalkov@yandex.ru>
2022-06-17 11:18:43 +0300

    first 1/6

---
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -0,0 +1,10 @@
+package sorting
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    while (scanner.hasNext()) {
+        val line = scanner.nextLine()
+        println("hi ${line}")
+    }
+}
