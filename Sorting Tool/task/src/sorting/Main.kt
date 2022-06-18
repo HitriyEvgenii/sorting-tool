77e0e8944355fc6761f9fbd2f5c4d5ff1d8e715f
HitriyEvgenii <moskalkov@yandex.ru>
2022-06-18 19:12:37 +0300

    first 1/6 done

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -3,8 +3,15 @@ import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
+    val numbersList = emptyList<Int>().toMutableList()
     while (scanner.hasNext()) {
-        val line = scanner.nextLine()
-        println("hi ${line}")
+        val line = scanner.nextLine().split("\\s+".toRegex())
+        //println(line)
+        numbersList += line.map {it.toString().toInt()}
+        //println(numbersList)
     }
+    val maxValue = numbersList.maxOrNull()
+    val maxCount = numbersList.count {max -> max == maxValue}
+    println("Total numbers: ${numbersList.size}")
+    println("The greatest number: ${maxValue} (${maxCount} time(s)).")
 }
