8550e4e1a5d8253f173b05853bef482e31c49bd2
HitriyEvgenii <moskalkov@yandex.ru>
2022-06-18 22:53:27 +0300

    first 2/6 first

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -1,17 +1,41 @@
 package sorting
 import java.util.Scanner
 
-fun main() {
+fun main(args: Array<String>) {
+    /*args ?: "word"
+    when {
+        args[2] == "word" -> typeWord()
+        args[0] == "long" -> typeInt()
+        args[1] == "line" -> typeLine()
+    }
+    */
+    typeWord()
+}
+
+fun typeInt() {
     val scanner = Scanner(System.`in`)
     val numbersList = emptyList<Int>().toMutableList()
     while (scanner.hasNext()) {
         val line = scanner.nextLine().split("\\s+".toRegex())
-        //println(line)
         numbersList += line.map {it.toString().toInt()}
-        //println(numbersList)
     }
     val maxValue = numbersList.maxOrNull()
     val maxCount = numbersList.count {max -> max == maxValue}
-    println("Total numbers: ${numbersList.size}")
+    println("Total numbers: ${numbersList.size}.")
     println("The greatest number: ${maxValue} (${maxCount} time(s)).")
 }
+
+fun typeWord() {
+    val scanner = Scanner(System.`in`)
+    val numbersList = emptyList<String>().toMutableList()
+    while (scanner.hasNext()) {
+        val line = scanner.nextLine().split("\\s+".toRegex())
+        numbersList += line
+    }
+    val maxValue = numbersList.maxWithOrNull (compareBy {it.length})
+    val maxCount = numbersList.count {max -> max == maxValue}
+    println("Total words: ${numbersList.size}.")
+    println("The longest word: ${maxValue} (${maxCount} time(s), ${((maxCount.toDouble() * 100) / numbersList.size).toInt()}%).")
+}
+
+fun typeLine() {}
